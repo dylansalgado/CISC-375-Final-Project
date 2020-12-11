2eeb9551ba28153c693c79885d9f724dc20bfd02
EmmaSinn <57824139+EmmaSinn@users.noreply.github.com>
2020-12-10 18:07:40 -0600

    Still have to ask how he wants the queries and finish the put

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let path = require('path');
 // NPM modules
 let express = require('express');
 let sqlite3 = require('sqlite3');
+const { start } = require('repl');
 
 
 let app = express();
@@ -48,11 +49,13 @@ app.get('/codes', (req, res) => {
         }
         var query = "SELECT * FROM Codes WHERE code = ?";
 
-        var i = 1;
+        let i = 1;
         for(i; i < list_of_codes.length; i++) {
             query = query + "OR code = ?";
         }
 
+        //clear the json out
+        codetypearr = [];
         db.all(query, list_of_codes, (err, rows) => {
             if(err) {
                 res.status(404).type("txt");
@@ -71,6 +74,9 @@ app.get('/codes', (req, res) => {
             }
         });
     } else {
+        //clear the json out
+        codetypearr = [];
+        //Default query
         db.all("SELECT * FROM Codes", req.params, (err, rows) => {
             if(err) {
                 res.status(404).type("txt");
@@ -97,35 +103,94 @@ var neighborhoodobj = [];
 app.get('/neighborhoods', (req, res) => {
     let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
     // db.all("SELECT * from Neighborhoods")
-    db.all("SELECT * FROM Neighborhoods ORDER BY neighborhood_name ASC", req.params, (err, rows) => {
-        if(err) {
-            res.status(404).type("txt");
-            res.write("Error executing SQL query");
-            res.end();
-        }
-        else {
-            console.log("Successfully read query");
-            var addedNums = [] 
-            for(var i in rows){
-                var item = rows[i];
-                var splitNeighborhood = String(item.neighborhood_name).split(" - ").pop();
-                var check = addedNums.includes(item.neighborhood_number);
+    if(req.originalUrl.includes("?id=")) {
+        //If want a specific code, something along these lines
+        //This gets everything after ?code=
+        var url_flags = req.originalUrl.split('=')[1];
+        var list_of_ids = [];
 
-                if(check == false){
-                    var insert = {
-                        "id" : item.neighborhood_number,
-                        "name" : splitNeighborhood
-                    }
-                    neighborhoodobj.push(insert);
-                }
-                addedNums.push(item.neighborhood_number);
-                
-            }
-            
-            res.status(200).type('json');
-            res.status(200).type('json').send(neighborhoodobj);
+        //If there are multiple codes, get all the codes
+        if(url_flags.includes(",")) {
+            list_of_ids = url_flags.split(",");
+            console.log(list_of_ids);
+        } else {
+            list_of_ids = url_flags;
         }
-    });
+        var query = "SELECT * FROM Neighborhoods WHERE neighborhood_number = ?";
+
+        var i = 1;
+        for(i; i < list_of_ids.length; i++) {
+            query = query + "OR neighborhood_number = ?";
+        }
+
+        query = query +  "ORDER BY neighborhood_name ASC";
+
+        //Clear the json out
+        neighborhoodobj = [];
+        db.all(query, list_of_ids, (err, rows) => {
+            if(err) {
+                res.status(404).type("txt");
+                res.write("Error executing SQL query");
+                res.end();
+            }
+            else {
+                console.log("Successfully read query");
+                var addedNums = [] 
+                for(var i in rows){
+                    var item = rows[i];
+                    var splitNeighborhood = String(item.neighborhood_name).split(" - ").pop();
+                    var check = addedNums.includes(item.neighborhood_number);
+
+                    if(check == false){
+                        var insert = {
+                            "id" : item.neighborhood_number,
+                            "name" : splitNeighborhood
+                        }
+                        neighborhoodobj.push(insert);
+                    }
+                    addedNums.push(item.neighborhood_number);
+                    
+                }
+                
+                res.status(200).type('json');
+                res.status(200).type('json').send(neighborhoodobj);
+            }
+        });
+
+    } else {
+        //Clear the json out
+        neighborhoodobj = [];
+        //Default query
+        db.all("SELECT * FROM Neighborhoods ORDER BY neighborhood_name ASC", req.params, (err, rows) => {
+            if(err) {
+                res.status(404).type("txt");
+                res.write("Error executing SQL query");
+                res.end();
+            }
+            else {
+                console.log("Successfully read query");
+                var addedNums = [] 
+                for(var i in rows){
+                    var item = rows[i];
+                    var splitNeighborhood = String(item.neighborhood_name).split(" - ").pop();
+                    var check = addedNums.includes(item.neighborhood_number);
+
+                    if(check == false){
+                        var insert = {
+                            "id" : item.neighborhood_number,
+                            "name" : splitNeighborhood
+                        }
+                        neighborhoodobj.push(insert);
+                    }
+                    addedNums.push(item.neighborhood_number);
+                    
+                }
+                
+                res.status(200).type('json');
+                res.status(200).type('json').send(neighborhoodobj);
+            }
+        });
+    }
 });
 
 // REST API: GET/incidents
@@ -133,43 +198,227 @@ app.get('/neighborhoods', (req, res) => {
 var incidents_json = [];
 app.get('/incidents', (req, res) => {
     let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
-    db.all("SELECT * FROM Incidents", req.params, (err, rows) => {
-        if(err) {
-            res.status(404).type("Error");
-            res.write("Error executing SQL query");
-            res.end();
+    if(req.originalUrl.includes("?")) {
+        //Handle the different params
+        //This will have everything after the ?
+        let params = req.originalUrl.split("?")[1];
+
+        //Different param options
+        let start_date;
+        let end_date;
+        let code; //This could be a list
+        let grid; //This could be a list
+        let neighborhood; //This could be a list
+        let limit;
+
+        //This will split the different params
+        let list_of_params = [];
+        if(params.includes("&")) {
+            //There are multiple parameters
+            list_of_params = params.split("&");
+        } else {
+            list_of_params = params;
         }
-        else {
-            console.log("Successfully read query");
-            // Make JSON object
-            /* EXAMPLE
-              {
-                "case_number": "19245020",
-                "date": "2019-10-30",
-                "time": "23:57:08", (if time includes date: 2019-05-009T05:00)
-                "code": 9954,
-                "incident": "Proactive Police Visit",
-                "police_grid": 87,
-                "neighborhood_number": 7,
-                "block": "THOMAS AV  & VICTORIA"
-              }
-            */
-            incidents_json = rows;
-            // split on T, will have to loop through the json
-            var i = 0;
-            for(i; i < incidents_json.length; i++) {
-                if(String(incidents_json[i]["time"]).includes("T")){
-                    //split on T
-                    time_temp = String(incidents_json[i]["time"].split("T"));
-                    incidents_json[i]["time"] = time_temp[1];
+
+        let i = 0;
+        for(i; i < list_of_params.length; i++) {
+            //split params into their categories
+            if(list_of_params[i].includes("start_date")) {
+                start_date = list_of_params[i].split("=")[1];
+            } else if(list_of_params[i].includes("end_date")) {
+                end_date = list_of_params[i].split("=")[1];
+            } else if(list_of_params[i].includes("code")) {
+                code = list_of_params[i].split("=")[1];
+            } else if(list_of_params[i].includes("grid")) {
+                grid = list_of_params[i].split("=")[1];
+            } else if(list_of_params[i].includes("neighborhood")) {
+                neighborhood = list_of_params[i].split("=")[1];
+            } else if(list_of_params[i].includes("limit")) {
+                limit = list_of_params[i].split("=")[1];
+            }
+        }
+
+        //Check to see if there are multiple inputs for certain categories
+        if(code.includes(",")) {
+            code = code.split(",");
+        }
+        if(grid.includes(",")) {
+            grid = grid.split(",");
+        }
+        if(neighborhood.includes(",")) {
+            neighborhood = neighborhood.split(",");
+        }
+
+        //Forming the query and params, if the thing is present, it adds to the query and parameters
+        //AND's or OR's, ask about this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        let query = "SELECT * FROM Incidents WHERE";
+        let params_list = [];
+        if(start_date) {
+            if(end_date) {
+                query = query + " (";
+            }
+            query = query + " date >= ?";
+            params_list.push(start_date);
+        }
+
+        if(end_date) {
+            if(params_list.length == 0) {
+                query = query + " date <= ?";
+            } else {
+                query = query + " AND date <= ? )";
+            }
+            params_list.push(end_date);
+        }
+
+        if(code) {
+            if(params_list.length == 0) {
+                let i = 1;
+                query = query + " code = ?";
+                for(i; i < code.length; i++) {
+                    query = query + " OR code = ?";
+                }
+            } else {
+                let i = 1;
+                //Is this an OR or an AND
+                query = query + "OR code = ?";
+                for(i; i < code.length; i++) {
+                    query = query + " OR code = ?";
                 }
             }
-
-            res.status(200).type('json');
-            res.status(200).type('json').send(incidents_json);
+            let i = 0;
+            for(i; i < code.length; i++) {
+                params_list.push(code[i]);
+            }
         }
-    });
-    
+
+        if(grid) {
+            if(params_list.length == 0) {
+                let i = 1;
+                query = query + " police_grid = ?";
+                for(i; i < grid.length; i++) {
+                    query = query + " OR police_grid = ?";
+                }
+            } else {
+                let i = 1;
+                //Is this an OR or an AND
+                query = query + " OR police_grid = ?";
+                for(i; i < grid.length; i++) {
+                    query = query + " OR police_grid = ?";
+                }
+            }
+            let i = 0;
+            for(i; i < grid.length; i++) {
+                params_list.push(grid[i]);
+            }
+        }
+
+        if(neighborhood) {
+            if(params_list.length == 0) {
+                let i = 1;
+                query = query + " neighborhood_number = ?";
+                for(i; i < neighborhood.length; i++) {
+                    query = query + " OR neighborhood_number = ?";
+                }
+            } else {
+                let i = 1;
+                //Is this an OR or an AND
+                query = query + " OR neighborhood_number = ?";
+                for(i; i < neighborhood.length; i++) {
+                    query = query + " OR neighborhood_number = ?";
+                }
+            }
+            let i = 0;
+            for(i; i < neighborhood.length; i++) {
+                params_list.push(neighborhood[i]);
+            }
+        }
+
+        if(limit) {
+            if(params_list.length == 0) {
+                query = query + " ";
+            }
+        }
+
+        //Clear the json
+        incidents_json = [];
+        db.all(query, list_of_params, (err, rows) => {
+            if(err) {
+                res.status(404).type("Error");
+                res.write("Error executing SQL query");
+                res.end();
+            }
+            else {
+                console.log("Successfully read query");
+                // Make JSON object
+                /* EXAMPLE
+                {
+                    "case_number": "19245020",
+                    "date": "2019-10-30",
+                    "time": "23:57:08", (if time includes date: 2019-05-009T05:00)
+                    "code": 9954,
+                    "incident": "Proactive Police Visit",
+                    "police_grid": 87,
+                    "neighborhood_number": 7,
+                    "block": "THOMAS AV  & VICTORIA"
+                }
+                */
+                incidents_json = rows;
+                // split on T, will have to loop through the json
+                var i = 0;
+                for(i; i < incidents_json.length; i++) {
+                    if(String(incidents_json[i]["time"]).includes("T")){
+                        //split on T
+                        time_temp = String(incidents_json[i]["time"].split("T"));
+                        incidents_json[i]["time"] = time_temp[1];
+                    }
+                }
+
+                res.status(200).type('json');
+                res.status(200).type('json').send(incidents_json);
+            }
+        });
+        
+    } else {
+        //Clear the json
+        incidents_json = [];
+        //Default query
+        db.all("SELECT * FROM Incidents", req.params, (err, rows) => {
+            if(err) {
+                res.status(404).type("Error");
+                res.write("Error executing SQL query");
+                res.end();
+            }
+            else {
+                console.log("Successfully read query");
+                // Make JSON object
+                /* EXAMPLE
+                {
+                    "case_number": "19245020",
+                    "date": "2019-10-30",
+                    "time": "23:57:08", (if time includes date: 2019-05-009T05:00)
+                    "code": 9954,
+                    "incident": "Proactive Police Visit",
+                    "police_grid": 87,
+                    "neighborhood_number": 7,
+                    "block": "THOMAS AV  & VICTORIA"
+                }
+                */
+                incidents_json = rows;
+                // split on T, will have to loop through the json
+                var i = 0;
+                for(i; i < incidents_json.length; i++) {
+                    if(String(incidents_json[i]["time"]).includes("T")){
+                        //split on T
+                        time_temp = String(incidents_json[i]["time"].split("T"));
+                        incidents_json[i]["time"] = time_temp[1];
+                    }
+                }
+
+                res.status(200).type('json');
+                res.status(200).type('json').send(incidents_json);
+            }
+        });
+    }
 });
 
 // REST API: PUT /new-incident
