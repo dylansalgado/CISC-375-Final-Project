20c8ca5d53aaa232e4e92c35c04d0b500190751a
EmmaSinn <57824139+EmmaSinn@users.noreply.github.com>
2020-12-15 13:01:10 -0600

    Page numbers work, just need to make it dynamic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -205,38 +205,49 @@ function retrieveData() {
             el: '#table',
             data: {
                 currentPage: 1,
-                elementsPerPage: 25,
+                elementsPerPage: 50,
                 ascending: false,
                 sortColumn: '',
-                rows: [data_complete],
-                methods: {
-                    "sortTable": function sortTable(col) {
-                        if (this.sortColumn === col) {
-                            this.ascending = !this.ascending;
-                        } else {
-                            this.ascending = true;
-                            this.sortColumn = col;
-                        }
+                rows: data_complete,
+            },
+            methods: {
+                "sortTable": function sortTable(col) {
+                    if (this.sortColumn === col) {
+                        this.ascending = !this.ascending;
+                    } else {
+                        this.ascending = true;
+                        this.sortColumn = col;
+                    }
                     
-                        var ascending = this.ascending;
+                    var ascending = this.ascending;
                 
-                        this.rows.sort(function(a, b) {
-                            if (a[col] > b[col]) {
+                    this.rows.sort(function(a, b) {
+                        if (a[col] > b[col]) {
                             return ascending ? 1 : -1
-                            } else if (a[col] < b[col]) {
+                        } else if (a[col] < b[col]) {
                             return ascending ? -1 : 1
-                            }
-                            return 0;
-                        })
-                    }
-                },
-                computed: {
-                    "columns": function columns() {
-                        if (this.rows.length == 0) {
-                            return [];
                         }
-                        return Object.keys(this.rows[0])
+                            return 0;
+                    })
+                },
+                "num_pages": function num_pages() {
+                    return Math.ceil(this.rows.length / this.elementsPerPage);
+                },
+                "get_rows": function get_rows() {
+                    var start = (this.currentPage-1) * this.elementsPerPage;
+                    var end = start + this.elementsPerPage;
+                    return this.rows.slice(start, end);
+                },
+                "change_page": function change_page(page) {
+                    this.currentPage = page;
+                }
+            },
+            computed: {
+                "columns": function columns() {
+                    if (this.rows.length == 0) {
+                        return [];
                     }
+                    return Object.keys(this.rows[0])
                 }
             }
         });
